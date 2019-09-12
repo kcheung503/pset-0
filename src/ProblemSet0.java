c00024312cf27db386206ef96c479ada877e8ca7
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2019-09-12 11:38:25 -0400

    First Commit

--- src/ProblemSet0.java
+++ src/ProblemSet0.java
@@ -1,85 +1,87 @@
 /**
  * Problem Set 0.
- * 
+ *
  * It's time to put your skills to the test. This problem set focuses on printing
  * text to the console, as well as identifying and correcting compilation and
  * runtime errors in the supplied program code.
- * 
+ *
  * The specifications for each exercise are outlined below. I've written most of
  * the functionality for you, but unfortunately, my code is riddled with mistakes.
  * It's your job to fix the errors I've made, so that the code meets the
  * requirements set forth in this section (as well as the Deliverables section).
- * 
+ *
  * Work through these exercises on your own. Experiment, make mistakes, ask
  * questions, and fix your mistakes. It's the only way to get good at programming.
  */
 
-Public class PSet0 {
+public class ProblemSet0 {
 
-    public void Main(String args) {
+    public static void Main(String[] args) {
 
         /*
          * Exercise 1.
-         * 
+         *
          * Print a message to the console.
          */
 
-        systemout.println(Hello, world! Am I a programmer yet?\n"
+        System.out.println("\nHello, world! Am I a programmer yet?\n");
 
         /*
          * Exercise 2.
-         * 
+         *
          * Print APCS in block letters to the console.
          */
 
-        System.out.println("   ##    ######  ######## ########")
-        System.out.println("  #  #   #     # #        #";
-        System.out.println(" #    #  #     # #       #");
-        Syste.out.print("######## ######  #        ########");
-        System.out.printf("#      # #       #               #");
+        System.out.println("   ##    ######  ######## ########");
+        System.out.println("  #  #   #     # #        #");
+        System.out.println(" #    #  #     # #        #");
+        System.out.println("######## ######  #        ########");
         System.out.println("#      # #       #               #");
-        Systemoutprintln("#      # #       ######## ########\n");
+        System.out.println("#      # #       #               #");
+        System.out.println("#      # #       ######## ########\n");
 
         /*
          * Exercise 3.
-         * 
+         *
          * Print a quoted message to the console.
          */
 
-        System.out.println(""The answer's quite simple," the professor said. "Just think outside the box!"\n
+        System.out.println("The answer's quite simple,\" the professor said. \"Just think outside the box!\"\n")
 
         /*
          * Exercise 4.
-         * 
+         *
          * Print a face to the console.
          */
 
-        Systemut.println("   \\\\\");
-        System.out.println("  +"""""+");
-        System.outprintln("(|  0 0  |)");
-        System.out.println(" |   ^   |")
-        Systemout.println(" |  '-'  |);
-        Systemout.println("  +-----+\n");
+        System.out.println("\n   \\\\\\\\\\");
+        System.out.println("  +\"\"\"\"\"+");
+        System.out.println("(|  0 0  |)");
+        System.out.println(" |   ^   |");
+        System.out.println(" |  '-'  |");
+        System.out.println("  +-----+\n ");
 
         /*
          * Exercise 5.
-         * 
+         *
          * Print a to-do list to the console.
          */
 
-        System.outprintln("Here's today's to-do list:\n- Go food shopping\\t- Mow the lawn\n")
+        System.out.println("Here's today's to-do list:");
+        System.out.println("\t- Go food shopping");
+        System.out.ptintln("\t- Mow the lawn\n");
 
         /*
          * Exercise 6.
-         * 
+         *
          * Print a square of text to the console.
          */
 
-        System.out.println("H E L L O\nE\\A\nL\tP\nL\t\tC\nO A P C S\n
+        System.out.println("H E L L O\nE\\A\nL\tP\nL\t\tC\nO A P C S\n")
 
         /*
          * Exercise 7.
-         * 
+         *
          * Print a countdown to the console.
          */
 
@@ -87,7 +89,7 @@ Public class PSet0 {
 
         /*
          * Exercise 8.
-         * 
+         *
          * Print three diamonds to the console.
          */
 
@@ -100,7 +102,7 @@ Public class PSet0 {
 
         /*
          * Exercise 9.
-         * 
+         *
          * Print an encouraging message to the console.
          */
 
@@ -108,7 +110,7 @@ Public class PSet0 {
 
         /*
          * Exercise 10.
-         * 
+         *
          * Print a plus sign to the console.
          */
 
